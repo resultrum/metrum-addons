d5571ab0f2243b0b0fe0b3a84351c9f5a1a378a7
Jonathan Nemry <jonathan.nemry@metrum.lu>
2018-06-18 17:41:41 +0200

    [WIP]

--- account_invoice_delivery_address/__manifest__.py
+++ account_invoice_delivery_address/__manifest__.py
@@ -9,13 +9,16 @@
         show a delivery address into the invoice too""",
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Jonathan Nemry <jonathan.nemry@metrum.lu>',
+    'author': 'Jonathan Nemry <jonathan.nemry@metrum.lu>,'
+    'Odoo Community Association (OCA)',
     'website': 'https://metrum.lu',
     'depends': [
         'base',
         'account',
+        'sale',
     ],
     'data': [
+        'views/account_invoice.xml',
     ],
     'demo': [
     ],
