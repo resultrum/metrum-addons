5dcde236a967a749a84925c518dbe350c838d1e5
Jonathan Nemry <jonathan.nemry@metrum.lu>
2018-06-18 16:59:27 +0200

    [ADD] new module account_invoice_delivery_address

---
+++ account_invoice_delivery_address/README.rst
@@ -0,0 +1,20 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================================
+Account Invoice Delivery Address
+================================
+
+If you have to invoice a customer about another address: show a delivery
+    address into the invoice too in order to validate the location where the inshow 
+
+Known issues / Roadmap
+======================
+
+/
+
+Contributors
+------------
+
+* Jonathan Nemry <jonathan.nemry@metrum.lu>
---
+++ account_invoice_delivery_address/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Jonathan Nemry <jonathan.nemry@metrum.lu>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Delivery Address',
+    'summary': """
+        If you have to invoice a customer about another address:
+        show a delivery address into the invoice too""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Jonathan Nemry <jonathan.nemry@metrum.lu>',
+    'website': 'https://metrum.lu',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
